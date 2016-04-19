8186ffc0502284465c69a14833b9cd1c895fbacc
Sho Otani <sho.otani@gmail.com>
2016-04-19 11:24:41 +0900

    apply style to textarea and table with dummy data

--- src/js/components/csvtable.js
+++ src/js/components/csvtable.js
@@ -12,7 +12,18 @@ export default React.createClass({
   },
   render() {
     return (
-      <div>CSV TABLE</div>
+      <div className="csv-table">
+        <textarea className="row-csv" readOnly></textarea>
+        <table>
+          <thead>
+            <tr><th>test1</th><th>test2</th></tr>
+          </thead>
+          <tbody>
+            <tr><td>val1</td><td>val2</td></tr>
+            <tr><td>val3</td><td>val4</td></tr>
+          </tbody>
+        </table>
+      </div>
     );
   }
 })

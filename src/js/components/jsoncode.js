04beb0648f5222091278a0045cd317199511f2c3
Sho Otani <sho.otani@gmail.com>
2016-04-19 17:54:03 +0900

    attach hide and show events to jsoncode component

--- src/js/components/jsoncode.js
+++ src/js/components/jsoncode.js
@@ -15,6 +15,21 @@ export default React.createClass({
     const val = e.target.value;
     this.setState({value: val});
   },
+  blurHandler(e) {
+    console.log("blur!");
+    this.setState({display: "_hidden"});
+  },
+  focusHandler(e) {
+    console.log("focus!");
+  },
+  clickHandler(e) {
+    console.log("click!");
+    this.setState({display: ""}, () => {
+      if(this.myTextInput !== null) {
+        this.myTextInput.focus();
+      }
+    });
+  },
   prettyJson(str) {
     const json = jsonFrom(str);
     if(json) {
@@ -28,11 +43,18 @@ export default React.createClass({
   render() {
     console.log("render!");
     const prettyJson = this.prettyJson(this.state.value);
-    const className = (this.state.display) ? "row-json "+this.state.display : "row-json" ;
+    const className1 = (this.state.display) ? "row-json "+this.state.display : "row-json" ;
+    const className2 = (!this.state.display) ? "pretty-json _hidden" : "pretty-json" ;
     return (
       <div className="json-code">
-        <textarea className={className} value={this.state.value} onChange={this.changeHandler} />
-        <pre class="pretty-json"><code dangerouslySetInnerHTML={{__html: prettyJson}} /></pre>
+        <textarea className={className1} value={this.state.value}
+          onChange={this.changeHandler}
+          onBlur={this.blurHandler}
+          ref={(ref) => this.myTextInput = ref}
+        />
+        <pre className={className2}
+          onClick={this.clickHandler}
+        ><code dangerouslySetInnerHTML={{__html: prettyJson}} /></pre>
       </div>
     );
   }

0249e325aa6d6ef0b2dd6eb6e8091f16698ed899
Sho Otani <sho.otani@gmail.com>
2016-04-19 12:46:09 +0900

    bind `.csv textarea` and `.csv-table textarea`

--- src/js/app.js
+++ src/js/app.js
@@ -6,10 +6,24 @@ import CsvTableView from "./components/csvtable";
 console.log("yo-ho-!");
 console.log(React);
 
+function cb(e) {
+  readDropFile(e);
+}
+
+const App = React.createClass({
+  componentDidMount() {
+  },
+  render() {
+    return (
+      <div>
+        <DropView contentType="json" callBack={cb} />
+        <CsvTableView />
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <div>
-    <DropView contentType="json" callBack={readDropFile} />
-    <CsvTableView />
-  </div>,
+  <App />,
   document.getElementById('react')
 );
--- src/js/components/csvtable.js
+++ src/js/components/csvtable.js
@@ -5,15 +5,21 @@ export default React.createClass({
   },
   getInitialState: () => {
     return {
+      rowCsv: document.querySelector(".csv textarea").value
     }
   },
-  componentDidMount() {
-    console.log("csvtable mounted!");
+  focusHandler() {
+    console.log("focused!");
+    this.setState({
+      rowCsv: document.querySelector(".csv textarea").value
+    });
   },
   render() {
+    console.log("render!");
+    console.log(this.props);
     return (
       <div className="csv-table">
-        <textarea className="row-csv" readOnly></textarea>
+        <textarea className="row-csv" value={this.state.rowCsv} onFocus={this.focusHandler} readOnly />
         <table>
           <thead>
             <tr><th>test1</th><th>test2</th></tr>

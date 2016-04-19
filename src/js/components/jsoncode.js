1caf3b0a2a29dd4b5271b5bc9bd91b1ecdf607f6
Sho Otani <sho.otani@gmail.com>
2016-04-19 17:25:21 +0900

    prittify json when input

--- src/js/components/jsoncode.js
+++ src/js/components/jsoncode.js
@@ -6,16 +6,33 @@ export default React.createClass({
   getInitialState: () => {
     return {
       json: {},
+      value: "",
       display: ""
     }
   },
+  changeHandler(e) {
+    console.log("changed!");
+    const val = e.target.value;
+    this.setState({value: val});
+  },
+  prettyJson(str) {
+    const json = jsonFrom(str);
+    if(json) {
+      const pretty = JSON.stringify(json, undefined, 2);
+      if (pretty.length < (50 * 1024)) {
+        return hljs.highlightAuto(pretty).value;
+      }
+    }
+    return null;
+  },
   render() {
     console.log("render!");
+    const prettyJson = this.prettyJson(this.state.value);
     const className = (this.state.display) ? "row-json "+this.state.display : "row-json" ;
     return (
       <div className="json-code">
-        <textarea className={className} value={this.state.rowCsv} />
-        <pre class="pretty-json"><code></code></pre>
+        <textarea className={className} value={this.state.value} onChange={this.changeHandler} />
+        <pre class="pretty-json"><code dangerouslySetInnerHTML={{__html: prettyJson}} /></pre>
       </div>
     );
   }

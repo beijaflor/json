047bfa8617732affefadbfc965738a3547cba634
Sho Otani <sho.otani@gmail.com>
2016-04-23 21:52:19 +0900

    hide download and showrow link when table has 0 rows

--- src/js/components/downloadDialog.babel.js
+++ src/js/components/downloadDialog.babel.js
@@ -10,18 +10,16 @@ export default React.createClass({
       return (
         <p>Your JSON will appear below as a table.</p>
       );
-    } else {
+    } else if (this.props.rows) {
       return (
         <p>
-          {(() => {
-            if(this.props.rows) {
-              return (
-                <span>Below are the first few rows (<span className="rows-count">{this.props.rows}</span> total). </span>
-              )
-            }
-          })()}
+          <span>Below are the first few rows (<span className="rows-count">{this.props.rows}</span> total). </span>
           <a download="result.csv" href="#" className="download">Download the entire CSV</a>, or <a href="#" className="raw"  onClick={this.props.showRowCsvtableHandler}>show the raw data</a>.
         </p>
+      );
+    } else {
+      return (
+        <p />
       );
     }
   }

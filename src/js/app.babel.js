a4681cb7f3cdfc6760a499b79ce083896790d00c
Sho Otani <sho.otani@gmail.com>
2016-04-25 11:45:01 +0900

    add navigation to switch j2c and c2j

--- src/js/app.babel.js
+++ src/js/app.babel.js
@@ -2,32 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { IndexRoute, Link, Router, Route, hashHistory, useRouterHistory } from "react-router";
+import { IndexRoute, Router, Route, hashHistory, useRouterHistory } from "react-router";
 import { createHashHistory } from "history";
 
 // setup redux
 import reducer from "./reducer";
-import App2 from "./container";
+import container from "./container";
+import container2 from "./container2";
 const store = createStore(reducer);
 
 console.log("yo-ho-!");
 
 // setup Router
 const history = useRouterHistory(createHashHistory)({queryKey: false});
+import Nav from "./components/nav";
 
 const App = React.createClass({
   render() {
     return (
-      <div><Link to="/csv">link</Link></div>
+      <div>
+        <Nav />
+        { this.props.children }
+      </div>
     );
   }
 });
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App} />
-      <Route path="/csv" component={App2} />
+  <Provider store={ store }>
+    <Router history={ history }>
+      <Route path="/" component={ App }>
+        <IndexRoute component={ container } />
+        <Route path="/csv" component={ container2 } />
+      </Route>
     </Router>
   </Provider>,
   document.getElementById('react')

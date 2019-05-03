c5853e6cf1d767e1cf7cd1f2e8887e70dfded0be
beijaflor <sho.otani@gmail.com>
2019-05-03 20:41:15 +0900

    fix error message

--- src/js/components/errorDialog.babel.js
+++ src/js/components/errorDialog.babel.js
@@ -9,8 +9,7 @@ export default React.createClass({
     if (this.props.error) {
       return (
         <div className="error">
-          There was an error parsing this JSON. If you're sure this JSON is valid, please <a className="report" target="_blank" href="https://github.com/konklone/json/issues/new"> file an issue</a>.<br />
-          You can <a className="save" href="#">create a permalink to the error</a> any time.
+          There was an error parsing this JSON. If you're sure this JSON is valid, please <a className="report" target="_blank" href="https://github.com/baijaflor/json/issues/new"> file an issue</a>.
         </div>
       );
     } else {
